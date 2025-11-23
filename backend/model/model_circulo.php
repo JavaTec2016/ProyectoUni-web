b5d2424593bbe780db3d53e782643556eaa49512
Santiago Banuelos <santiago.bt@tecjerez.edu.mx>
2025-11-23 13:55:09 -0600

    Modelo de clase, arreglados parametros del modelo de circulo

--- backend/model/model_circulo.php
+++ backend/model/model_circulo.php
@@ -8,7 +8,7 @@ class Circulo extends Modelo
     public final const NOMBRE = "nombre";
     public final const MONTO_MINIMO = "monto_minimo";
 
-    public function __construct(int|null $id = null, int|null $nombre = null, string|null $monto_minimo = null)
+    public function __construct(int|null $id = null, string|null $nombre = null, float|null $monto_minimo = null)
     {
         $this->fillValuesFrom(array_keys(static::$rules), func_get_args());
     }
---
+++ backend/model/model_clase.php
@@ -0,0 +1,22 @@
+<?php
+
+class Clase extends Modelo
+{
+    public static $rules = [];
+
+    public final const ID = "id";
+    public final const ANIO_GRADUCION = "anio_graduacion";
+
+    public function __construct(int|null $id = null, int|null $anio_graduacion = null)
+    {
+        $this->fillValuesFrom(array_keys(static::$rules), func_get_args());
+    }
+    public static function setRules()
+    {
+        static::addRule(self::ID, new DataRow("int", "INT", true, true, false, -1, -1, true, Modelo::REGEX_INTEGER));
+        static::addRule(self::ANIO_GRADUCION, new DataRow("string", "STRING", true, false, false, -1, -1, true, Modelo::REGEX_INTEGER));
+    }
+}
+
+Clase::setRules();
+Models::set("clase", new Clase());
