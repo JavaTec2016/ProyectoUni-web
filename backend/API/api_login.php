e9b083c4d095c0a07c7e6027f03b82ece4722fc3
Santiago Banuelos <santiago.bt@tecjerez.edu.mx>
2025-11-26 16:20:13 -0600

    Revertida autenticacion de apis, errores corregidos en api de login

--- backend/API/api_login.php
+++ backend/API/api_login.php
@@ -1,8 +1,6 @@
 <?php
 include_once(__DIR__ . '/../../database/conexion_bd.php');
 header("Content-Type: application/json");
-if (!isset($_SESSION) || !$_SESSION['autenticado']) die(0);
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     $con = new ConexionBD([
@@ -12,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $cadenaJSON = file_get_contents('php://input');
     $datos = array();
     if (!isset($cadenaJSON) || $cadenaJSON == false) {
-        $datos = $_POST;
+        $datos = $_GET;
     } else $datos = json_decode($cadenaJSON, true);
 
     $usr = $datos["usuario"];
@@ -22,8 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $p_cifrado = hash('sha256', $pass);
 
     $sql = "SELECT * FROM usuario WHERE nombre='$u_cifrado' AND pass='$p_cifrado'";
-    $res = mysqli_query($conexion, $sql);
-    $data = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $res = mysqli_query($con->getConexion(), $sql);
+    $data = mysqli_fetch_all($res, MYSQLI_ASSOC)[0];
     $num = mysqli_num_rows($res);
 
     $response = array("status"=>false, "rol"=>"");
--- backend/API/api_mysql_altas.php
+++ backend/API/api_mysql_altas.php
@@ -1,6 +1,5 @@
 <?php
 include_once(__DIR__ . '/../controller/funcion_altas.php');
-if (!isset($_SESSION) || !$_SESSION['autenticado']) die(0);
 header("Content-Type: application/json");
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){ //agregar
