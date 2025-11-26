1042581d9409456eb056e50fd068dfec7c5baced
Santiago Banuelos <santiago.bt@tecjerez.edu.mx>
2025-11-26 17:36:00 -0600

    solucion de errores en API de autenticacion

--- backend/API/api_login.php
+++ backend/API/api_login.php
@@ -15,19 +15,17 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     $usr = $datos["usuario"];
     $pass = $datos["pass"];
-
     $u_cifrado = hash('sha256', $usr);
     $p_cifrado = hash('sha256', $pass);
 
     $sql = "SELECT * FROM usuario WHERE nombre='$u_cifrado' AND pass='$p_cifrado'";
     $res = mysqli_query($con->getConexion(), $sql);
-    $data = mysqli_fetch_all($res, MYSQLI_ASSOC)[0];
     $num = mysqli_num_rows($res);
-
     $response = array("status"=>false, "rol"=>"");
 
     if ($num == 1) {
         session_start();
+        $data = mysqli_fetch_all($res, MYSQLI_ASSOC)[0];
         $_SESSION["autenticado"] = true;
         $_SESSION["usuario"] = $usr;
         $_SESSION["rol"] = $data["rol"];
